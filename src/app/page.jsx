0d89270f8cd1be618408caadd63f432a5234d21d
G0then <nelsonsilva19999@gmail.com>
2023-03-14 00:17:28 +0000

    Add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,16 @@
-import HomeHeader from '@/components/HomeHeader'
-import React from 'react'
+import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
     <>
-     <HomeHeader/>
+      <HomeHeader />
+      <div className="flex flex-col items-center justify-center mt-24">
+       <Image src="https://www.google.pt/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png" width="300" height="100" alt="Google Banner"/>
+       <HomeSearch/>
+      </div>
     </>
-  )
+  );
 }
